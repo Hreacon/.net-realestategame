86c9a4b2f6779cabf57d07c4cb2cd9dc46cd967f
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-04-28 10:12:19 -0700

    add unit testing for player

---
+++ RealEstateGameTests/PlayerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RealEstateGame.Models;
+using Xunit;
+
+namespace RealEstateGameTests
+{
+    public class PlayerTest
+    {
+        [Fact]
+        public void PlayerCanGenerate()
+        {
+
+            var player = GetPlayer();
+            Assert.Equal("test", player.Username);
+            Assert.Equal(1000.00, player.Money); // generated in the GeneratePlayer() method
+        }
+
+        [Fact]
+        public void PlayerCanUseActions()
+        {
+            var player = GetPlayer();
+            Assert.Equal(2,player.Actions); // player starts with two actions
+            player.UseAction();
+            Assert.Equal(1,player.Actions); // player used an action now has 1
+        }
+
+        [Fact]
+        public void PlayerChangesTurns()
+        {
+            var player = GetPlayer();
+            Assert.Equal(0, player.TurnNum);
+            player.Actions = 0;
+            player.NextTurn();
+            Assert.Equal(1, player.TurnNum);
+            player.UseAction();
+            player.UseAction();
+            Assert.Equal(2, player.TurnNum);
+        }
+
+        public Player GetPlayer()
+        {
+            var username = "test";
+            var id = "thisisanid";
+            Player player = Player.GeneratePlayer(new ApplicationUser()
+            {
+                UserName = username,
+                Id = id
+            });
+            return player;
+        }
+    }
+}
---
+++ RealEstateGameTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RealEstateGameTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RealEstateGameTests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0f2139d7-46db-4c63-8109-06d2f13b6e77")]
