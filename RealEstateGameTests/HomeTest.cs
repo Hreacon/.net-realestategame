ea3f4071785a953dea290446cd0ec9bd79d8ee46
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-04-28 10:36:07 -0700

    add test for home model

--- RealEstateGameTests/HomeTest.cs
+++ RealEstateGameTests/HomeTest.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RealEstateGame.Models;
+using Xunit;
 
 namespace RealEstateGameTests
 {
     // This project can output the Class library as a NuGet Package.
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public class Class1
+    public class HomeTest
     {
-        public Class1()
+        public HomeTest()
         {
+        }
+
+        [Fact]
+        public void HomeCanGenerate()
+        {
+            var home = Home.GenerateHome(1, new Random());
+            Assert.Equal(1, home.PlayerId);
         }
     }
 }
